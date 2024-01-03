3a95a3b3ec3c3cfb63d12e20c34c98fbfc2d8edf
Shivam <shivamsdeshmukh1107@gmail.com>
2024-01-03 15:42:23 +0530

    /api/question changes

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -44,11 +44,14 @@ export async function POST(req: Request, res: Response) {
       },
     });
 
-    const { data } = await axios.post(`${process.env.API_URL}/api/questions`, {
-      amount,
-      topic,
-      type,
-    });
+    const { data } = await axios.post(
+      `${process.env.API_URL as string}/api/questions`,
+      {
+        amount,
+        topic,
+        type,
+      }
+    );
 
     if (type === "mcq") {
       type mcqQuestion = {
@@ -116,113 +119,57 @@ export async function POST(req: Request, res: Response) {
   }
 }
 
-// export async function POST(req: Request, res: Response) {
-//   try {
-//     const session = await getAuthSession();
-//     if (!session?.user) {
-//       return NextResponse.json(
-//         { error: "You must be logged in to create a game." },
-//         {
-//           status: 401,
-//         }
-//       );
-//     }
-//     const body = await req.json();
-//     const { topic, type, amount } = QuizCreationSchema.parse(body);
-//     const game = await prisma.game.create({
-//       data: {
-//         gameType: type,
-//         timeStarted: new Date(),
-//         userId: session.user.id,
-//         topic,
-//       },
-//     });
-//     await prisma.topic_count.upsert({
-//       where: {
-//         topic,
-//       },
-//       create: {
-//         topic,
-//         count: 1,
-//       },
-//       update: {
-//         count: {
-//           increment: 1,
-//         },
-//       },
-//     });
+export async function GET(req: Request, res: Response) {
+  try {
+    const session = await getAuthSession();
+    if (!session?.user) {
+      return NextResponse.json(
+        { error: "You must be logged in to create a game." },
+        {
+          status: 401,
+        }
+      );
+    }
+    const url = new URL(req.url);
+    const gameId = url.searchParams.get("gameId");
+    if (!gameId) {
+      return NextResponse.json(
+        { error: "You must provide a game id." },
+        {
+          status: 400,
+        }
+      );
+    }
 
-//     const { data } = await axios.post(
-//       `${process.env.API_URL as string}/api/questions`,
-//       {
-//         amount,
-//         topic,
-//         type,
-//       }
-//     );
+    const game = await prisma.game.findUnique({
+      where: {
+        id: gameId,
+      },
+      include: {
+        questions: true,
+      },
+    });
+    if (!game) {
+      return NextResponse.json(
+        { error: "Game not found." },
+        {
+          status: 404,
+        }
+      );
+    }
 
-//     if (type === "mcq") {
-//       type mcqQuestion = {
-//         question: string;
-//         answer: string;
-//         option1: string;
-//         option2: string;
-//         option3: string;
-//       };
-
-//       const manyData = data.questions.map((question: mcqQuestion) => {
-//         // mix up the options lol
-//         const options = [
-//           question.option1,
-//           question.option2,
-//           question.option3,
-//           question.answer,
-//         ].sort(() => Math.random() - 0.5);
-//         return {
-//           question: question.question,
-//           answer: question.answer,
-//           options: JSON.stringify(options),
-//           gameId: game.id,
-//           questionType: "mcq",
-//         };
-//       });
-
-//       await prisma.question.createMany({
-//         data: manyData,
-//       });
-//     } else if (type === "open_ended") {
-//       type openQuestion = {
-//         question: string;
-//         answer: string;
-//       };
-//       await prisma.question.createMany({
-//         data: data.questions.map((question: openQuestion) => {
-//           return {
-//             question: question.question,
-//             answer: question.answer,
-//             gameId: game.id,
-//             questionType: "open_ended",
-//           };
-//         }),
-//       });
-//     }
-
-//     return NextResponse.json({ gameId: game.id }, { status: 200 });
-//   } catch (error) {
-//     if (error instanceof z.ZodError) {
-//       return NextResponse.json(
-//         { error: error.issues },
-//         {
-//           status: 400,
-//         }
-//       );
-//     } else {
-//       return NextResponse.json(
-//         { error: "An unexpected error occurred." },
-//         {
-//           status: 500,
-//         }
-//       );
-//     }
-//   }
-// }
+    return NextResponse.json(
+      { game },
+      {
+        status: 400,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An unexpected error occurred." },
+      {
+        status: 500,
+      }
+    );
+  }
+}
