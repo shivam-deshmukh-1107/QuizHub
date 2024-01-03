e64bf999bcffd26ed39772e9ab3eda4428df7621
Shivam <shivamsdeshmukh1107@gmail.com>
2024-01-03 23:29:21 +0530

    get func

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -119,57 +119,57 @@ export async function POST(req: Request, res: Response) {
   }
 }
 
-export async function GET(req: Request, res: Response) {
-  try {
-    const session = await getAuthSession();
-    if (!session?.user) {
-      return NextResponse.json(
-        { error: "You must be logged in to create a game." },
-        {
-          status: 401,
-        }
-      );
-    }
-    const url = new URL(req.url);
-    const gameId = url.searchParams.get("gameId");
-    if (!gameId) {
-      return NextResponse.json(
-        { error: "You must provide a game id." },
-        {
-          status: 400,
-        }
-      );
-    }
+// export async function GET(req: Request, res: Response) {
+//   try {
+//     const session = await getAuthSession();
+//     if (!session?.user) {
+//       return NextResponse.json(
+//         { error: "You must be logged in to create a game." },
+//         {
+//           status: 401,
+//         }
+//       );
+//     }
+//     const url = new URL(req.url);
+//     const gameId = url.searchParams.get("gameId");
+//     if (!gameId) {
+//       return NextResponse.json(
+//         { error: "You must provide a game id." },
+//         {
+//           status: 400,
+//         }
+//       );
+//     }
 
-    const game = await prisma.game.findUnique({
-      where: {
-        id: gameId,
-      },
-      include: {
-        questions: true,
-      },
-    });
-    if (!game) {
-      return NextResponse.json(
-        { error: "Game not found." },
-        {
-          status: 404,
-        }
-      );
-    }
+//     const game = await prisma.game.findUnique({
+//       where: {
+//         id: gameId,
+//       },
+//       include: {
+//         questions: true,
+//       },
+//     });
+//     if (!game) {
+//       return NextResponse.json(
+//         { error: "Game not found." },
+//         {
+//           status: 404,
+//         }
+//       );
+//     }
 
-    return NextResponse.json(
-      { game },
-      {
-        status: 400,
-      }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: "An unexpected error occurred." },
-      {
-        status: 500,
-      }
-    );
-  }
-}
+//     return NextResponse.json(
+//       { game },
+//       {
+//         status: 400,
+//       }
+//     );
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "An unexpected error occurred." },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// }
