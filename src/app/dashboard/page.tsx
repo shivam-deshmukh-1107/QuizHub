780237bd7c18bffa7d5b64dd4da031634aafba02
Shivam <shivamsdeshmukh1107@gmail.com>
2024-01-03 21:22:22 +0530

    detailsdialog page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import HotTopicsCard from "./HotTopicsCard";
 import RecentActivityCard from "./RecentActivityCard";
+import DetailsDialog from "@/components/DetailsDialog";
 
 type Props = {};
 
@@ -22,7 +23,7 @@ const Dashboard = async (props: Props) => {
     <main className="p-8 mx-auto max-w-7xl">
       <div className="flex items-center">
         <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
-        {/* <DetailsDialog /> */}
+        <DetailsDialog />
       </div>
       <div className="grid gap-4 mt-4 md:grid-cols-2">
         <QuizMeCard />
---
+++ src/components/DetailsDialog.tsx
@@ -0,0 +1,145 @@
+"use client";
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Github, HelpCircle } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { LinkedInLogoIcon } from "@radix-ui/react-icons";
+
+type Props = {};
+
+const DetailsDialog = (props: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <span className="flex items-center px-2 py-1 text-white rounded-md bg-slate-800">
+          What is this
+          <HelpCircle className="w-5 h-5 ml-1" />
+        </span>
+      </DialogTrigger>
+      <DialogContent className="w-[70vw] max-w-[100vw] md:w-[50vw]">
+        <DialogHeader>
+          <DialogTitle className="text-2xl">Welcome to QuizHub!</DialogTitle>
+          <DialogDescription>
+            <div className="flex items-center gap-3 my-2">
+              <p className="flex items-center">
+                <LinkedInLogoIcon className="w-5 h-5" />
+                <Link
+                  className="ml-1 underline"
+                  href="https://www.linkedin.com/in/shivam-deshmukh-880512191/"
+                >
+                  LinkedIn
+                </Link>
+              </p>
+              <p className="flex items-center">
+                <Github className="w-5 h-5" />
+                <Link
+                  className="ml-1 underline"
+                  href="https://github.com/shivam-deshmukh-1107/QuizHub"
+                >
+                  GitHub
+                </Link>
+              </p>
+            </div>
+            <p className="my-2 mt-4 ">
+              Embark on an exhilarating journey to redefine your quiz
+              experience! Say goodbye to the ordinary and embrace the
+              extraordinary on QuizHub! Our platform is revolutionizing quizzes
+              and trivia, unlocking a world of possibilities through the
+              captivating magic of artificial intelligence. Elevate your fun –
+              join QuizHub now!
+            </p>
+            <hr />
+            <p className="my-2 font-semibold">
+              <h4 className="text-base font-semibold">Built with</h4>
+              <div className="grid justify-around grid-cols-4 mt-2 gap-y-3">
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="planetscale"
+                    src="/planetscale.png"
+                    width={35}
+                    height={35}
+                  />
+                  <span className="">Planet Scale</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="nextjs"
+                    src="/nextjs.png"
+                    width={35}
+                    height={35}
+                  />
+                  <span className="">Next.js</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="tailwind"
+                    src="/tailwind.png"
+                    width={35}
+                    height={35}
+                  />
+                  <span className="">Tailwind</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="nextauth"
+                    src="/nextauth.png"
+                    width={30}
+                    height={30}
+                  />
+                  <span className="">NextAuth</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="openai"
+                    src="/openai.png"
+                    width={30}
+                    height={30}
+                  />
+                  <span className="">OpenAI</span>
+                </div>
+
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="react query"
+                    src="/react-query.png"
+                    width={30}
+                    height={30}
+                  />
+                  <span className="">React Query</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="primsa"
+                    src="/prisma.png"
+                    width={30}
+                    height={30}
+                  />
+                  <span className="">Prisma</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image
+                    alt="typescript"
+                    src="/typescript.png"
+                    width={30}
+                    height={30}
+                  />
+                  <span className="">TypeScript</span>
+                </div>
+              </div>
+            </p>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DetailsDialog;
